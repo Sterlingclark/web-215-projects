b7fdcc526140fd156149433da96a54b8b43e277b
Sterling Clark <sterlingdclark13@gmail.com>
2021-09-13 19:01:01 -0400

    Moved inner docBlock and changed movie array

--- clark-sterling-proj3/js/clark-sterling-proj3.js
+++ clark-sterling-proj3/js/clark-sterling-proj3.js
@@ -4,7 +4,12 @@
  * @param {Number} tipPercent - the amount you want to tip
  * @param {Number} people -the number of people spliting the bill
  * @returns {String} A String that Displays the tipCalculator Function
- */
+ * 
+ *  * This is the Movie ovject
+ * @param {String} title -title of the movie
+ * @param {Number} year -year the movie came out
+ * @param {Boolean} favorite -Boolean if this is your favorite movie
+**/
 
 function tipCalculator(bill,tipPercent,people) {
   let tip = (tipPercent / 100) * bill;
@@ -17,34 +22,21 @@ console.log(tipCalculator(62.75, 17, 4));
 console.log(tipCalculator(105.85, 20, 3));
 console.log(tipCalculator(85.98, 18, 2));
 
-/**
- * This is the Movie ovject
- * @param {String} title -title of the movie
- * @param {Number} year -year the movie came out
- * @param {Boolean} favorite -Boolean if this is your favorite movie
- */
-
 function Movie(title, year, favorite) {
   this.title = title;
   this.year = year;
   this.favorite = favorite;
-  
-  /**
-   * A function to change the boolean value of favorite movie
-   */
   this.toggleFavorite = function() {
     this.favorite = !this.favorite;
   }
 }
 
-var movies = [
-  new Movie("Raiders of the Lost Arc", 1981, false),
-  new Movie("The Thing", 1982, false),
-  new Movie("Midsommer", 2019, true),
-]
+movie1 = new Movie("Raiders of the Lost Arc", 1981, false);
+movie2 =  new Movie("The Thing", 1982, false);
+movie3 = new Movie("Midsommer", 2019, true);
 
-console.log(movies[0]);
-console.log(movies[1]);
-console.log(movies[2]);
-console.log(movies[1].toggleFavorite());
-console.log(movies[1]);
+console.log(Movie("Raiders of the Lost Arc", 1981, false));
+console.log(movie2);
+console.log(movie3);
+console.log(movie2.toggleFavorite());
+console.log(movie2);
