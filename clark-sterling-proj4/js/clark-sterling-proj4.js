f2c3ec11839ae2618ab2986d2ef145d2bf5731b3
Sterling Clark <sterlingdclark13@gmail.com>
2021-09-18 20:22:19 -0400

    Changed h1 text and first paragraph element

---
+++ clark-sterling-proj4/js/clark-sterling-proj4.js
@@ -0,0 +1,5 @@
+// Change the inner HTML of the h1 element
+document.querySelector("h1").innerHTML = "Congratulations!";
+
+// Change the first paragraph to a BLOCKQUOTE and changed the text
+document.querySelector("p").outerHTML = "<blockquote>You have JavaScript.</blockquote>"
