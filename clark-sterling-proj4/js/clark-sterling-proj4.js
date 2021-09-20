b3b406752ba5d9e0e2dd2fbb0553d6cc9032b9e9
Sterling Clark <sterlingdclark13@gmail.com>
2021-09-20 17:18:48 -0400

    Added variable for query selector of h1 element and took out sanity check for TARGET

--- clark-sterling-proj4/js/clark-sterling-proj4.js
+++ clark-sterling-proj4/js/clark-sterling-proj4.js
@@ -1,5 +1,7 @@
+//Variables
+let firstH1 = document.querySelector("h1");
 // Change the inner HTML of the H1 element
-document.querySelector("h1").innerHTML = "Congratulations!";
+firstH1.textContent = "Congratulations!";
 
 // Change the first paragraph to a BLOCKQUOTE and changed the text
 document.querySelector("p").outerHTML = "<blockquote>You have JavaScript.</blockquote>";
@@ -7,17 +9,13 @@ document.querySelector("p").outerHTML = "<blockquote>You have JavaScript.</block
 // Add the target attribute to anchor tags
 const MDNLINK = document.querySelector(".externalLink");
 
-if (MDNLINK.hasAttribute("target")) {
-  console.log(MDNLINK.getAttribute("target"));
-} else {
-  MDNLINK.setAttribute("target", "_blank");
-}
+MDNLINK.setAttribute("target", "_blank");
 
 // Change the H1 to display as uppercase
-document.querySelector("h1").style.textTransform = "uppercase";
+firstH1.style.textTransform = "uppercase";
 
 // Change css properties on the BLOCKQUOTE
-document.querySelector("blockquote").style.cssText = "background-color: #ffff80; border: solid 5px #ff0000; padding: 1rem; ";
+document.querySelector("blockquote").style.cssText = "background-color: #ff8; border: solid 5px #f00; padding: 1rem; ";
 
 // Change classes for the anchor tag
 MDNLINK.classList.remove("importantLink");
