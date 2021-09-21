d89cbd49e182317920c79ede5d75fb4a50a0fa74
Sterling Clark <sterlingdclark13@gmail.com>
2021-09-21 14:52:16 -0400

    Started project 5

---
+++ clark-sterling-proj5/js/p5-part1.js
@@ -0,0 +1,38 @@
+// For reference - The Beatles breakup announcement was made April 10, 1970
+
+/*
+Set a date in the past (The Beatles breakup date) and log it to the console
+As the date won't change, it's a constant
+*/
+
+
+/*
+Extract the year, month by number, date, and day of week by number and log them to the console
+Be sure to log the correct month number. For example, January = 1, not 0
+*/
+
+
+/*
+Append a leading 0 to the month number for January through September as those month numbers have a single digit. Log the new number to the console.
+*/
+
+
+/*
+Convert the month number to a month name (long name, not an abbreviation) and log it to the console
+*/
+
+
+/*
+Convert the day number to a day name (long name, not an abbreviation) and log it to the console
+*/
+
+
+/* 
+Use variables you already have to build a string for the breakup date like this:
+Friday, April 10, 1970
+*/
+
+
+/*
+Create a <b> element, populate its text with the string above, and place it on the page after the hardcoded <br> tag inside the hardcoded paragraph. If you are cringing because you think the <b> tag is deprecated, read the explanation in Part 2 of this project.
+*/
