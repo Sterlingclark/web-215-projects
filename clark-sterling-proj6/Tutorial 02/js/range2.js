ab0c5776655850db9294bb4541601af85ea6610d
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-02 14:29:10 -0400

    Finished tutorial 2

--- clark-sterling-proj6/Tutorial 02/js/range2.js
+++ clark-sterling-proj6/Tutorial 02/js/range2.js
@@ -1,7 +1,22 @@
 const SLIDER = document.querySelector('input');
 const PARA = document.querySelector('p');
+const CURRENTSIZE = document.createElement('div');
+const DEFSIZE = SLIDER.getAttribute('value') + 'px';
+CURRENTSIZE.textContent = DEFSIZE;
+PARA.insertAdjacentElement('beforebegin', CURRENTSIZE);
+document.querySelector('body').appendChild(CURRENTSIZE);
 SLIDER.addEventListener('mousemove', function(){
   let textSize = this.value;
   PARA.style.fontSize = textSize + 'px';
+  CURRENTSIZE.textContent = textSize;
 });
 
+const MINSIZE = SLIDER.getAttribute('min');
+const MAXSIZE = SLIDER.getAttribute('max');
+const MINEL = document.createElement('span');
+MINEL.textContent = MINSIZE +'px';
+SLIDER.insertAdjacentElement('beforebegin', MINEL);
+
+const MAXEL = document.createElement('span');
+MAXEL.textContent = MAXSIZE + 'px';
+SLIDER.insertAdjacentElement('afterend', MAXEL);
