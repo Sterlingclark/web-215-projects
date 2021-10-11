b11c4de2a68ab6d79a58de078f7c278e35e4cbe1
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-11 01:14:47 -0400

    finished working on initialize functions adding event functions nex

--- clark-sterling-proj7/js/events.js
+++ clark-sterling-proj7/js/events.js
@@ -1,6 +1,12 @@
 /*******************************************
 Page Initialization Function Calls
 *******************************************/
+showElement('#thumb-roundness-control', 'block');
+showElement('#large-roundness-control', 'block');
+showElement('#description-control', 'block');
+hideElements('#descriptions > div');
+showElement('#descriptions > div:first-of-type', 'block');
+cancelLinks('a');
 
 
 /*******************************************
@@ -11,9 +17,41 @@ Event Function Calls
 /*******************************************
 Initialize Page Functions
 *******************************************/
-
+/**
+ * 
+ * @param {String} tag - pass in the anchor tag to prevent them from opening. 
+ */
+function cancelLinks(tag) {
+  var links = document.querySelectorAll(tag);
+  for (var i =0; i < links.length; i++) {
+    links[i].addEventListener('click', function(event)
+    { event.preventDefault(); }, false);
+  }
+}
+/**
+ * 
+ * @param {String} id - id of the div surronding the tag you want to show.
+ * @param {String} html - the html tag of what you want shown.
+ */
+function showElement(id, style) {
+  document.querySelector(id).style.display = style;
+}
+/**
+ * 
+ * @param {String} elem - the element that you want to hide. 
+ */
+function hideElements(elem) {
+  let divs = document.querySelectorAll(elem);
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].style.display = 'none';
+  }
+}
 
 /*******************************************
 Event Functions
 *******************************************/
+
+function swapImage(thumbNail, large, event) {
+  
+}
 
