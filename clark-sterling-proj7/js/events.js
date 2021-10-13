afc0d207f3d2797eaa2fb6a8c18ec5a4277876dd
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-13 00:42:36 -0400

    Added swapImage and swapDescription functions working on roundCorners and togggleElement next

--- clark-sterling-proj7/js/events.js
+++ clark-sterling-proj7/js/events.js
@@ -12,7 +12,10 @@ cancelLinks('a');
 /*******************************************
 Event Function Calls
 *******************************************/
+swapImage('#thumbs > a', '#full-size > img', 'mouseover');
 
+
+toggleElement('#discription-control > span', '#descriptions', 'Show Descriptions', 'Hide Descriptions');
 
 /*******************************************
 Initialize Page Functions
@@ -52,6 +55,41 @@ Event Functions
 *******************************************/
 
 function swapImage(thumbNail, large, event) {
-  
+  let thumbs = document.querySelectorAll(thumbNail);
+  let fullSize = document.querySelector(large);
+  for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener(event, function() {
+      let newSrc = this.getAttribute('href');
+      fullSize.setAttribute('src', newSrc);
+    });
+  }
+}
+
+function swapDescriptions(thumbNail, event, container, attribute) {
+  hideElements('#descriptions > div');
+  let thumbs = document.querySelectorAll(thumbNail);
+  let match = document.querySelector();
+  let swap = document.querySelector(container);
+  for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener(event, function() {
+      
+      
+    });
+  }
+}
+
+function roundCorners(slider, roundElem) {
+
+}
+
+function toggleElement(bttn, des,) {
+  /*
+  set visibility flag = true
+  get toggle button 
+  get the big box of all descriptions 
+  add click listener to the toggle button
+    if big box is visible, hide it and change the buttons text
+    if the big box is hidden, show it and change the buttons text
+  */
 }
 
