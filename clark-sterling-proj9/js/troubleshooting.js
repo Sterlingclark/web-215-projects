15e8717750cf81304c646cbd336c445acb03dfa1
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-24 14:42:12 -0400

    got pyramid function to work, now adding the ability to make multi char pyramids

---
+++ clark-sterling-proj9/js/troubleshooting.js
@@ -0,0 +1,51 @@
+makePyramid(10, '^');
+makePyramid(10, '*');
+makePyramid(10, '+');
+
+makePyramid(10, '^-^');
+
+/**
+ * 
+ * @param {INT} rows - The number of rows in the pyramid
+ * @param {String} character - The character you want the pyramid made out of
+ */
+function makePyramid(rows, character) {
+  for(i = 1; i <= rows; i++){
+    const DIV = document.createElement("div");
+
+    for(c = 1; c <= (rows - i); c++){
+      DIV.innerHTML += "&nbsp;";
+    }
+
+    for(j = 1; j <= i; j++) {
+      DIV.innerHTML += character + "&nbsp;";
+    }
+
+    document.querySelector("body").appendChild(DIV);
+  }
+  
+  document.querySelector("body").innerHTML += '<hr>';
+  }
+
+  /**
+ * 
+ * @param {INT} rows - The number of rows in the pyramid
+ * @param {String} character - The characters you want the pyramid made out of
+ */
+  function makeMultiCharPyramid(rows, characters) {
+    for(i = 1; i <= rows; i++){
+      const DIV = document.createElement("div");
+  
+      for(c = 1; c <= (rows - i); c++){
+        DIV.innerHTML += "&nbsp;";
+      }
+  
+      for(j = 1; j <= i; j++) {
+        DIV.innerHTML += characters + "&nbsp;";
+      }
+  
+      document.querySelector("body").appendChild(DIV);
+    }
+    
+    document.querySelector("body").innerHTML += '<hr>';
+    }
