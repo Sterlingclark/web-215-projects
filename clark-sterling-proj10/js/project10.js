713c6f1c381ee418421cd1fe15126837939cbaa6
Sterling Clark <sterlingdclark13@gmail.com>
2021-10-31 11:50:56 -0400

    Started project 10. Changed for loop to foreach and changed to template literals

---
+++ clark-sterling-proj10/js/project10.js
@@ -0,0 +1,42 @@
+createEmail(); 
+
+function createEmail() {
+  const EMAILS = document.querySelectorAll('.email');
+  EMAILS.forEach(function(email, index){
+    const USER = email.dataset.user;
+    const DOMAIN = email.dataset.domain;
+    const A = document.createElement('a');
+    const ADDRESS = `${USER}@${DOMAIN}`;
+    A.setAttribute('href', 'mailto:', ADDRESS);
+    A.textContent = ADDRESS;
+    email.textContent = '';
+    email.appendChild(A);
+  });
+}
+
+
+
+
+let foods = [
+  'apple',
+  'banana',
+  'durian'
+]
+
+// for(i = 0; i < foods.length; i++){
+//   console.log(foods[i]);
+// }
+
+
+
+// foods.forEach(showMembers);
+// function showMembers(member, index, arr) {
+//   console.log(member);
+//   console.log(index);
+//   console.log(arr);
+//   console.log('-----------');
+// }
+
+foods.forEach(function(arrMember) {
+  console.log(`${arrMember} is a real tasty fruit`);
+});
